a5d85b9efc9008ce9827a2701e4e96358079aed6
damian gwozdz <gwozdziu1017@o2.pl>
2022-11-19 23:03:17 +0100

    Added implementation to IO class

--- include/IO.h
+++ include/IO.h
@@ -9,7 +9,46 @@ class IO
 {
 public:
     template <typename T>
-    static void print(T input);
+    static void print(const T& input);
+    template <typename container>
+    static void printContainer(const container& input);
+    template <typename container>
+    static void printContainer_pretty(const container& input);
     template <typename T>
     static T get();
+};
+
+/* Prints given argument to standard output */
+template <typename T>
+inline void IO::print(const T& input)
+{
+    std::cout << input;
+}
+
+/* Prints elements from given container without spaces*/
+template <typename container>
+inline void IO::printContainer(const container& input)
+{
+    for(auto elem : input)
+        IO::print(elem);
+}
+
+/* Prints elements from given container with spaces between elements*/
+template <typename container>
+inline void IO::printContainer_pretty(const container& input)
+{
+    for(auto elem : input)
+    {
+        IO::print(elem);
+        IO::print(" ");
+    }
+}
+
+/* Returns from standard input */
+template <typename T>
+inline T IO::get()
+{
+    T temp;
+    std::cin >> temp;
+    return temp;
 }
